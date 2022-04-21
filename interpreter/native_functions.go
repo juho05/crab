dcd3ef3eb9b482cebff0c10e1427ab55fb7199cb
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-04-21 21:30:38 +0200

    Implement input() and millis() native functions

--- interpreter/native_functions.go
+++ interpreter/native_functions.go
@@ -1,18 +1,25 @@
 package interpreter
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
+	"time"
 )
 
 func (e *Environment) registerNativeFunctions() {
 	e.Define("print", funcPrint{})
 	e.Define("println", funcPrintln{})
+	e.Define("input", funcInput{})
+	e.Define("millis", funcMillis{})
 }
 
 func registerNativeFunctions(m map[string]variableState) {
 	m["print"] = variableStateUsed
 	m["println"] = variableStateUsed
+	m["input"] = variableStateUsed
+	m["millis"] = variableStateUsed
 }
 
 type funcPrint struct{}
@@ -35,4 +42,27 @@ func (p funcPrintln) ArgumentCount() int {
 func (f funcPrintln) Call(i *interpreter, args []any) (any, error) {
 	fmt.Println(strings.TrimSuffix(strings.TrimSuffix(fmt.Sprintln(args...), "\n"), "\r"))
 	return nil, nil
+}
+
+type funcInput struct{}
+
+func (p funcInput) ArgumentCount() int {
+	return 1
+}
+
+func (f funcInput) Call(i *interpreter, args []any) (any, error) {
+	fmt.Print(args[0])
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text(), nil
+}
+
+type funcMillis struct{}
+
+func (p funcMillis) ArgumentCount() int {
+	return 0
+}
+
+func (p funcMillis) Call(i *interpreter, args []any) (any, error) {
+	return time.Now().UnixMilli(), nil
 }
